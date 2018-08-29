8df6a60df41498e4b3b565f1bd535ed71a8b0240
SkylineBin <xyliubin@outlook.com>
2018-08-29 12:09:55 +0800

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// service-worker.js
+// service-worker.js 
 
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
@@ -8,27 +8,36 @@ workbox.core.setCacheNameDetails({
     runtime: 'runtime-cache'
 });
 
-// let Service Worker take control of pages ASAP
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-// let Workbox handle our precache list
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-// use `networkFirst` strategy for `*.html`, like all my posts
 workbox.routing.registerRoute(
     /\.html$/,
     workbox.strategies.networkFirst()
 );
 
-// use `cacheFirst` strategy for images
 workbox.routing.registerRoute(
     /assets\/(img|icons)/,
     workbox.strategies.cacheFirst()
 );
 
-// third party files
+workbox.routing.registerRoute(
+    /^https?:\/\/cloud.netlifyusercontent.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
+
 workbox.routing.registerRoute(
     /^https?:\/\/cdn.staticfile.org/,
+    workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+    /^https?:\/\/at.alicdn.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/store.skylinebin.com/,
     workbox.strategies.staleWhileRevalidate()
 );
